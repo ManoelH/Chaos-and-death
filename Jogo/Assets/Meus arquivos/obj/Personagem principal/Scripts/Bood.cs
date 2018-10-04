64fb7ecbdf30c948f11a31c8d41ec209ff54f1aa
Manoel <henriqe115@gmail.com>
2018-10-04 13:33:30 -0300

    pequenas alterações na arma e blood

---
+++ Jogo/Assets/Meus arquivos/obj/Personagem principal/Scripts/Bood.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bood : MonoBehaviour {
+
+    private GameObject player;
+    private Animator animator;
+    // Use this for initialization
+
+    void Start () {
+        player = GameObject.FindWithTag("Player");
+        animator = GetComponent<Animator>();
+        animator.StopPlayback();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (player.GetComponent<Player>().life < 1){
+            animator.Play("bood");
+            //Destroy(player);
+
+        }
+    }
+}
